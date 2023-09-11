39e124e26d8cc5c009629605fbda76e473ebd962
buvanenko <buvanenko@vk.com>
2023-09-11 13:53:18 +0300

    init

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0xe0542D698E8d15A6B8D3cB14f3075F65473578Bd";
+export const tokenContractAddress =
+  "0xb09b20014A60041BBCE02497ff5454ceA456bE12";
+export const stakingContractAddress =
+  "0xe91715407BB63b4C257d95212EC029D64c69c796";
---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      {/* Top Section */}
+      <h1 className={styles.h1}>☭ Soviet Girls Staking</h1>
+      <div className={styles.nftBoxGrid}>
+        <div
+          className={styles.optionSelectBox}
+          role="button"
+          onClick={() => router.push(`/stake`)}
+        >
+          {/* Staking an NFT */}
+          <Image src="/icons/token.webp" alt="token" width={64} height={64} />
+          <h2 className={styles.selectBoxTitle}>Войти в кабинет стейкинга</h2>
+          <p className={styles.selectBoxDescription}>
+            Отправьте ваши NFT из коллекции Soviet Girls на хранение и получайте награды!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
